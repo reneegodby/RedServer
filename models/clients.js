bc3273b8a95b02942efc609147c2bb435efab362
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-01-22 18:11:34 -0500

    updated client model

--- models/clients.js
+++ models/clients.js
@@ -2,14 +2,26 @@ const { DataTypes } = require("sequelize");
 const db = require("../db");
 
 const Clients = db.define("comments", {
-  id: {
+  clientId: {
     type: DataTypes.UUID,
     primaryKey: true,
     defaultValue: DataTypes.UUIDV4,
     allowNull: false,
   },
-  content: {
+  firstName: {
     type: DataTypes.TEXT,
+    required: true,
+  },
+  lastName: {
+    type: DataTypes.TEXT,
+    required: true,
+  },
+  phoneNumber: {
+    type: DataTypes.NUMBER,
+    required: true,
+  },
+  address: {
+    type: DataTypes.STRING,
     required: true,
   }
 });
