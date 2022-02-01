236a7f370cc0baa09572103e00ed2ac961770916
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-01 18:21:19 -0500

    changed url

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT
+    dialect: 'postgres'
 })
 
 module.exports = sequelize;
