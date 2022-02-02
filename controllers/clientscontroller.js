a9a68b92df5098beaa81ded09e28246fc2cd2369
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-01 20:48:11 -0500

    working on delete endpoints

--- controllers/clientscontroller.js
+++ controllers/clientscontroller.js
@@ -1,63 +1,53 @@
 const Express = require("express");
 const router = Express.Router();
-const { models } = require('../models');
+const { models } = require("../models");
 let validateJWT = require("../middleware/validate-session");
 
-router.post('/client', validateJWT, async (req, res) => {
-    const {firstName,lastName, phoneNumber, address, notes} = req.body.clients;
+router.post("/client", validateJWT, async (req, res) => {
+  const { firstName, lastName, phoneNumber, address, notes } = req.body.clients;
 
-    try {
-        await models.Clients.create({
-            // clientId: clientId,
-            firstName,
-            lastName,
-            phoneNumber,
-            address,
-            notes,
-            userId: req.user.id
-        })
-        .then(
-            post => {
-                res.status(201).json({
-                    post: post,
-                    message: 'client created'
-                });
-            }
-        )
-    } catch (err){
-        res.status(500).json({
-            error: `Failed to create client: ${err}`
-        });
-    };
+  try {
+    await models.Clients.create({
+      // clientId: clientId,
+      firstName,
+      lastName,
+      phoneNumber,
+      address,
+      notes,
+      userId: req.user.id,
+    }).then((post) => {
+      res.status(201).json({
+        post: post,
+        message: "client created",
+      });
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: `Failed to create client: ${err}`,
+    });
+  }
 });
 
-
-
-
-
 router.delete("/delete/:clientId", validateJWT, async (req, res) => {
-    const clientId = req.params.id;
-    const { id } = req.user;
-    const query = {
-        where: {
-            id: clientId,
-            userId: id
-        }
-    };
-    try {
-        const deleteClient = await models.Clients.destroy(query);
-        res.status(200).json({
-            message: `${deleteClient} Client successfully deleted!`,
-            query: query
-        });
-    } catch (err) {
-        res.status(500).json({ error: err });
-        message = "Error deleting client";
-    }
-}
-);
-
-
+  const clientId = req.params.id;
+  const { id } = req.user;
+  const query = {
+    where: {
+      id: clientId,
+      userId: id,
+    },
+  };
+  try {
+    const deleteClient = await models.Clients.destroy(query);
+    res.status(200).json({
+      message: `${deleteClient} Client successfully deleted!`,
+      query: query,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err });
+    message = "Error deleting client";
+  }
+});
 
 module.exports = router;
 
@@ -74,7 +64,7 @@ module.exports = router;
 //     };
 
 //     const updatedLog = {
-//         
+//
 //     };
 //     console.log(updatedLog);
 
