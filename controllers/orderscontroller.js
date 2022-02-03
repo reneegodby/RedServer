830cb1f11a16a2d99d7204f8c63da9969bd967a7
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-02 19:16:00 -0500

    deletes are working!

--- controllers/orderscontroller.js
+++ controllers/orderscontroller.js
@@ -1,68 +1,58 @@
 const Express = require("express");
 const router = Express.Router();
-const { models } = require('../models');
+const { models } = require("../models");
 let validateJWT = require("../middleware/validate-session");
-// const client = require("pg/lib/native/client");
 
-
-router.post('/order',validateJWT, async (req, res) => {
-    const {typeOfOrder, quantity, dueDate, price,notes, image } = req.body.orders;
-    const {clientId} = req.body.orders;
-    try{
-        const setClientId = await models.Clients.findOne({
-            where: {
-                id: clientId,
-                userId: req.user.id
-            }
-        })
-        await models.Orders.create({
-            // orderId: orderId, 
-            typeOfOrder, 
-            quantity, 
-            dueDate, 
-            price, 
-            notes,
-            image,
-            clientId: setClientId.id
-        })
-        .then(
-            post => {
-                res.status(201).json({
-                    post: post,
-                    message: 'order created'
-                });
-            }
-        )
-    } catch (err){
-        console.log(err)
-        res.status(500).json({
-            error: `Failed to create order: ${err}`
-        });
-    };
-
+router.post("/order", validateJWT, async (req, res) => {
+  const { typeOfOrder, quantity, dueDate, price, notes, image } =
+    req.body.orders;
+  const { clientId } = req.body.orders;
+  try {
+    const setClientId = await models.Clients.findOne({
+      where: {
+        id: clientId,
+        userId: req.user.id,
+      },
+    });
+    await models.Orders.create({
+      typeOfOrder,
+      quantity,
+      dueDate,
+      price,
+      notes,
+      image,
+      clientId: setClientId.id,
+    }).then((post) => {
+      res.status(201).json({
+        post: post,
+        message: "order created",
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: `Failed to create order: ${err}`,
+    });
+  }
 });
 router.delete("/delete/:orderId", validateJWT, async (req, res) => {
-    const orderId = req.params.orderId;
-    const {id} = req.order;
-    const query = {
-        where: {
-          orderId: id,
-          id: orderId
-        },
-      };
-  
-    // const id = JSON.parse(JSON.stringify(query)).clientId;
-   
-    try {
-      const deleteOrder = await models.Orders.destroy(query);
-      res.status(200).json({
-        message: `${deleteOrder} Order successfully deleted!`,
-        query: query,
-      });
-    } catch (err) {
-      res.status(500).json({ error: err });
-      message = "Error deleting order";
-    }
-  });
+  const orderId = req.params.orderId;
+  const query = {
+    where: {
+      orderId: orderId,
+    },
+  };
+
+  try {
+    const deleteOrder = await models.Orders.destroy(query);
+    res.status(200).json({
+      message: `${deleteOrder} Order successfully deleted!`,
+      query: query,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err });
+    message = "Error deleting order";
+  }
+});
 
 module.exports = router;
