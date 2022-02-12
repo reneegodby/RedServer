97cfde84cdb2839c4df5d1733095f386f0214ece
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-12 14:28:30 -0500

    added options

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const dbConnection = require('./db');
-const controllers  = require('./controllers');
-const middleware = require('./middleware');
+const express = require("express");
+const dbConnection = require("./db");
+const controllers = require("./controllers");
+const middleware = require("./middleware");
 
 const app = express();
-
+app.options("*", cors());
 app.use(middleware.CORS);
 app.use(express.json());
-app.use('/auth', controllers.userscontroller);
-app.use('/orders', controllers.orderscontroller);
-app.use('/clients', controllers.clientscontroller);
+app.use("/auth", controllers.userscontroller);
+app.use("/orders", controllers.orderscontroller);
+app.use("/clients", controllers.clientscontroller);
 app.use(middleware.validateSession);
 
-dbConnection.authenticate()
-    .then(() => dbConnection.sync())
-    // .then(() => dbConnection.sync({force: true}))
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log(`[Server]: App is listening on ${process.env.PORT}.`)
-        })
-    })
-    .catch((err) => {
-        console.log(`[Server]: Server crashed. Error = ${err}`)
-    })
+dbConnection
+  .authenticate()
+  .then(() => dbConnection.sync())
+  // .then(() => dbConnection.sync({force: true}))
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`[Server]: App is listening on ${process.env.PORT}.`);
+    });
+  })
+  .catch((err) => {
+    console.log(`[Server]: Server crashed. Error = ${err}`);
+  });
