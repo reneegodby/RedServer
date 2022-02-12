20cc0f836aac81f0a763941df23af5f83a46fd9f
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-12 11:20:13 -0500

    test

--- db.js
+++ db.js
@@ -15,19 +15,5 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 module.exports = sequelize;
 
-// const Sequelize = require('sequelize');
-// // console.log(process.env)
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-
-//     dialect: 'postgres',
-
-//     dialectOptions: 
-//         process.env.ENV === 'local' ? {} : {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
-// })
 
 
