a7fef4258afbba5bbebc9c4cd638511a20386ddf
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-12 10:51:05 -0500

    forgot to comment in dialectOptions

--- db.js
+++ db.js
@@ -3,12 +3,13 @@ const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     host: process.env.DATABASE_HOST,
     dialect: 'postgres',
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
+    //comment in on heroku. 
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 
 })
 
