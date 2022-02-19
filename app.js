338b1fee91882c5b1581e9c9ec07017c52cee6be
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-18 21:26:17 -0500

    CORS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors"); 
 const dbConnection = require("./db");
 const controllers = require("./controllers");
-// const middleware = require("./middleware");
+const middleware = require("./middleware");
 
 // app.options("*", cors());
 app.use(cors())
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/auth", controllers.userscontroller);
 app.use("/orders", controllers.orderscontroller);
 app.use("/clients", controllers.clientscontroller);
-// app.use(middleware.validateSession);
+app.use(middleware.validateSession);
 
 dbConnection
   .authenticate()
