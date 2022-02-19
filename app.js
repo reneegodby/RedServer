9f0c2e6963cc8df2ad4dfa055c6a8663b106e732
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-18 21:10:57 -0500

    CORS Errors

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
-var cors = require("cors"); 
 const express = require("express");
+const app = express();
+const cors = require("cors"); 
 const dbConnection = require("./db");
 const controllers = require("./controllers");
-const middleware = require("./middleware");
+// const middleware = require("./middleware");
 
-const app = express();
-app.options("*", cors());
-app.use(middleware.CORS);
+// app.options("*", cors());
+app.use(cors())
+// app.use(middleware.CORS);
 app.use(express.json());
 app.use("/auth", controllers.userscontroller);
 app.use("/orders", controllers.orderscontroller);
 app.use("/clients", controllers.clientscontroller);
-app.use(middleware.validateSession);
+// app.use(middleware.validateSession);
 
 dbConnection
   .authenticate()
   .then(() => dbConnection.sync())
-  // .then(() => dbConnection.sync({force: true}))
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log(`[Server]: App is listening on ${process.env.PORT}.`);
