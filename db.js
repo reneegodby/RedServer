f84beec938f14e7559e898fe92adcd74d9f19127
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-18 23:38:19 -0500

    ssl

--- db.js
+++ db.js
@@ -4,12 +4,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     // host: process.env.DATABASE_HOST,
     dialect: 'postgres',
     //comment in on heroku. 
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 
 })
 
