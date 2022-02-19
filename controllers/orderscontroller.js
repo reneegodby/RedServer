a04c97cd2483ccf870506292da21584bb22bc22b
reneegodby <57304584+reneegodby@users.noreply.github.com>
2022-02-18 23:58:43 -0500

    test

--- controllers/orderscontroller.js
+++ controllers/orderscontroller.js
@@ -4,7 +4,6 @@ const { models } = require("../models");
 let validateJWT = require("../middleware/validate-session");
 const Clients = require("../models/clients");
 
-
 //CREATE ORDER
 router.post("/order/:id", validateJWT, async (req, res) => {
   const { typeOfOrder, quantity, dueDate, price, notes, image } =
@@ -42,38 +41,36 @@ router.post("/order/:id", validateJWT, async (req, res) => {
 
 //UPDATE ORDER
 router.put("/update/:orderId", validateJWT, async (req, res) => {
-  const { typeOfOrder, quantity, dueDate, price, notes, image } = req.body.orders;
+  const { typeOfOrder, quantity, dueDate, price, notes, image } =
+    req.body.orders;
   const orderId = req.params.orderId;
   const query = {
-      where: {
-          orderId: orderId,
-          
-      }
+    where: {
+      orderId: orderId,
+    },
   };
   const updatedOrder = {
-      
-      typeOfOrder: typeOfOrder,
-      quantity: quantity,
-      dueDate: dueDate,
-      price: price,
-      notes: notes,
-      image: image
+    typeOfOrder: typeOfOrder,
+    quantity: quantity,
+    dueDate: dueDate,
+    price: price,
+    notes: notes,
+    image: image,
   };
   console.log(updatedOrder);
 
   try {
-      const update = await models.Orders.update(updatedOrder, query);
-      res.status(200).json({
-          message: `${update} Order successfully updated!`,
-          update: updatedOrder,
-          query: query
-      });
+    const update = await models.Orders.update(updatedOrder, query);
+    res.status(200).json({
+      message: `${update} Order successfully updated!`,
+      update: updatedOrder,
+      query: query,
+    });
   } catch (err) {
-      res.status(500).json({ error: err });
-      message = "Error updating order";
+    console.log(err)
+    res.status(500).json({ error: err });
   }
-}
-);
+});
 
 //DELETE ORDER
 router.delete("/delete/:orderId", validateJWT, async (req, res) => {
@@ -98,21 +95,20 @@ router.delete("/delete/:orderId", validateJWT, async (req, res) => {
 
 //GET ALL ORDERS FOR SPECIFIC USER
 
-router.get('/', validateJWT, async (req, res) => {
+router.get("/", validateJWT, async (req, res) => {
   const { id } = req.user;
   try {
-      const orders = await models.Orders.findAll({
-          where: {
-              userId: id
-          }, include: [{model: Clients}]
-      });
-      res.status(200).json(orders);
-          message `${orders} Orders successfully retrieved!`
+    const orders = await models.Orders.findAll({
+      where: {
+        userId: id,
+      },
+      include: [{ model: Clients }],
+    });
+    res.status(200).json(orders);
+    message`${orders} Orders successfully retrieved!`;
   } catch (err) {
-      res.status(500).json({ error: err });
+    res.status(500).json({ error: err });
   }
-})
-
-
+});
 
 module.exports = router;
